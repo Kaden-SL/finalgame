eb5a134e7d9c29f48586be1dc17c25a35d8c9b2e
Kaden-SL <126031371+Kaden-SL@users.noreply.github.com>
2023-06-07 21:18:15 -0700

    basic ass model and player in place

--- palyer.gd
+++ palyer.gd
@@ -3,6 +3,8 @@ extends CharacterBody3D
 
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
+const GRAVITY = 1
+const MAX_FALL_SPEED=30
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
