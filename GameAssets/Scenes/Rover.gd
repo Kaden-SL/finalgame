aec6cc5d25222209aeec2df73be2be064acd07ec
Kaden-SL <126031371+Kaden-SL@users.noreply.github.com>
2023-06-14 18:58:03 -0700

    fixxed buttons

--- GameAssets/Scenes/Rover.gd
+++ GameAssets/Scenes/Rover.gd
@@ -22,6 +22,7 @@ func _physics_process(delta):
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
 		velocity.x = direction.x * SPEED
+		
 
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
---
+++ GameAssets/Scenes/roverbody.gd
@@ -0,0 +1,11 @@
+extends MeshInstance3D
+
+
+# Called when the node enters the scene tree for the first time.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+#	if Input. is_action_pressed("ui_right"):
+#		eg
+	
